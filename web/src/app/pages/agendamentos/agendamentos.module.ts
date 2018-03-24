54a6bb95658aebbf6af34b30ce0e0ddaff06d3fc
Yves Toupe <styvesamson@gmail.com>
2018-03-24 02:47:06 -0300

    Add:  calendar template module

--- web/src/app/pages/agendamentos/agendamentos.module.ts
+++ web/src/app/pages/agendamentos/agendamentos.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
-
 import { ThemeModule } from '../../@theme/theme.module';
 import { AgendamentosRoutingModule, routedComponents } from './agendamentos-routing.module';
 import {AgendamentoService} from './service/agendamento.service';
 import { TextMaskModule } from 'angular2-text-mask';
+import { CalendarModule } from 'angular-calendar';
 
 @NgModule({
   imports: [
+    CalendarModule.forRoot(),
     ThemeModule,
     AgendamentosRoutingModule,
     Ng2SmartTableModule,
